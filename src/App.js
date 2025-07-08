2f8bbc99baded6f9435c803e267ba7471ce0e6ff
sitholezw <evidencefelixy11@gmail.com>
2025-07-08 10:15:31 +0200

    Add newsletter subscription section to App component

--- src/App.js
+++ src/App.js
@@ -282,6 +282,13 @@ function App() {
                 <button type="submit">Send</button>
               </form>
             </section>
+            <section className="newsletter-section">
+              <h2>Subscribe to our Newsletter</h2>
+              <form action="https://YOUR_MAILCHIMP_URL" method="post" target="_blank">
+                <input type="email" name="EMAIL" placeholder="Your email" required />
+                <button type="submit">Subscribe</button>
+              </form>
+            </section>
           </>
         )}
       </main>
